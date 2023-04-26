68635b59b3b44542e0bc029553d16c9e822e8bba
Darvin Rodriguez <darvin.rod10@gmail.com>
2023-04-25 23:15:25 -0400

    Deleting the JOptionPane

--- src/main/java/gestor_app/Main.java
+++ src/main/java/gestor_app/Main.java
@@ -5,10 +5,10 @@ import gestor_app.UIMenu.Menu;
 
 public class Main {
     public static void main(String[] args) {
-         JOptionPane.showMessageDialog(null,
-                "This is a TO-DO app.\n Select an option",
-                "Welcome to my app \n" ,
-                JOptionPane.PLAIN_MESSAGE);
+//         JOptionPane.showMessageDialog(null,
+//                "This is a TO-DO app.\n Select an option",
+//                "Welcome to my app \n" ,
+//                JOptionPane.PLAIN_MESSAGE);
 
 
         System.out.println("Welcome to my app");
